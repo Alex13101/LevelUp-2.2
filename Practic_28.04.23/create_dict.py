3a4b0655b9a80d6eb6d4521806526218b2541266
Alex13101 <podiyachiy-a@mail.ru>
2023-05-02 15:06:47 +0300

    Генерация словаря(ключ-значение) и switch-функция

---
+++ Practic_28.04.23/create_dict.py
@@ -0,0 +1,31 @@
+lst = [1, 2, 3, 4, -5, 6, 7, 8, 9]
+
+new_list = {el: el ** 2 for el in lst}
+print(new_list)
+for k, v in new_list.items():
+   print(f' Квадрат числа {k} равен  {v}')
+
+
+def func_1():
+   print('Функция 1')
+
+def func_2():
+   print('Функция 2')
+
+def func_3():
+   print('Функция 3')
+
+
+switcher = {
+   1: func_1,
+   2: func_2,
+   3: func_3
+}
+
+def switch(case):
+   func = switcher.get(case, lambda: print('Неверный выбор'))
+   func()
+
+switch(input('Введите номер функции: ')) # Выводит функию 1
+#switch(2) # Выводит функию 2
+switch(4) # Выводит Неверный выбор
