bfe3820da0c0cfbaa3df5860387d6dc6afa595ca
Alex13101 <podiyachiy-a@mail.ru>
2023-05-07 19:48:43 +0300

    Работа с exel файлами

---
+++ Practic 5.05.23/work_exel.py
@@ -0,0 +1,16 @@
+import openpyxl
+
+workbook = openpyxl.load_workbook('example.xlsx')
+worksheet = workbook.active
+
+# worksheet['A1'] = 'Name'
+# worksheet['B1'] = 'Age'
+# worksheet['C1'] = 'City'
+#
+# worksheet.append(['John', 30, 'New York'])
+# worksheet.append(['Jane', 25, 'San Francisco'])
+
+workbook.save('example.xlsx')
+
+for row in worksheet.iter_rows(values_only=True):
+    print(row)
